d368281dc3c692080dd591b1f7d73c56847dda2d
juanpablocastillorioja <jnpbl92@gmail.com>
2024-01-25 11:21:01 -0400

    fix: fix form zoom issue

--- src/components/BreakingForm.tsx
+++ src/components/BreakingForm.tsx
@@ -35,7 +35,7 @@ export const BreakingForm = () => {
           <div className="flex gap-4">
             <div className="mb-6">
               <label htmlFor="success" className="block mb-2 text-base font-medium text-white">First name</label>
-              <Field name="firstName" className="border bg-white text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed " />
+              <Field name="firstName" className="border bg-white text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed " />
               {errors.firstName && touched.firstName
                 ? (
                   <div className="mt-2 text-sm text-red-600 dark:text-red-500">{errors.firstName}</div>
@@ -44,7 +44,7 @@ export const BreakingForm = () => {
             </div>
             <div className="mb-6">
               <label htmlFor="success" className="block mb-2 text-base font-medium text-white">Last name</label>
-              <Field name="lastName" className="border bg-white text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed " />
+              <Field name="lastName" className="border bg-white text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed " />
               {errors.lastName && touched.lastName
                 ? (
                   <div className="mt-2 text-sm text-red-600 dark:text-red-500">{errors.lastName}</div>
